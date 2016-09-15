1d3f8d9af4986a7e1b43e6e9eed3858b5c4f1bf3
Anthony Persaud <apersaud@gmail.com>
2016-09-15 10:09:01 -0700

    Updates hooks.

--- webhooks/hooks/user.rb
+++ webhooks/hooks/user.rb
@@ -1,13 +1,13 @@
 
 # Sample before_save hook for Parse::User class ("_User")
-Parse::Webhooks.route :before_save, :User do
+Parse::Webhooks.route :before_save, "_User" do
   u = parse_object
   puts "Before Save for username: #{user_object.username}"
   u
 end
 
 # Sample after_save hook for Parse::User class ("_User")
-Parse::Webhooks.route :after_save, :User do
+Parse::Webhooks.route :after_save, "_User" do
   u = parse_object
   puts "After Save for username: #{user_object.username}"
   u
