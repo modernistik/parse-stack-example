aa5df623bc6d01fd655090844775f71fada856a3
Anthony Persaud <persaud@modernistik.com>
2017-05-09 13:13:06 -0700

    Fixes typo.

--- webhooks/hooks/user.rb
+++ webhooks/hooks/user.rb
@@ -2,13 +2,13 @@
 # Sample before_save hook for Parse::User class ("_User")
 Parse::Webhooks.route :before_save, "_User" do
   u = parse_object
-  puts "Before Save for username: #{user_object.username}"
+  puts "Before Save for username: #{u.username}"
   u
 end
 
 # Sample after_save hook for Parse::User class ("_User")
 Parse::Webhooks.route :after_save, "_User" do
   u = parse_object
-  puts "After Save for username: #{user_object.username}"
+  puts "After Save for username: #{u.username}"
   u
 end
